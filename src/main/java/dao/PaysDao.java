1c05a17315ed71dbc688a3faac0c6cd5367f478b
Adorax <jeremy.maret@gmail.com>
2018-03-21 23:03:41 +0100

    Refactor for maven

--- src/main/java/dao/PaysDao.java
+++ src/main/java/dao/PaysDao.java
@@ -2,7 +2,7 @@ package dao;
 
 import java.util.ArrayList;
 import java.util.StringTokenizer;
-import outils.FileStr;
+//import outils.FileStr;
 import domaine.Pays;
 
 public class PaysDao {
@@ -10,12 +10,13 @@ public class PaysDao {
     private static final String FICHIER_PAYS = "Pays.txt";
 
     public static ArrayList getListePays() {
-        String[] tabPays = FileStr.read(FICHIER_PAYS);
-        ArrayList aLst = new ArrayList();
+        String[] tabPays = new String[3]; //FileStr.read(FICHIER_PAYS);
+        tabPays[0] = "ok";
+        /*ArrayList aLst = new ArrayList();
         for (int i=0; i<tabPays.length; i++) {
             StringTokenizer strTPays = new StringTokenizer(tabPays[i], ";");
-            aLst.add(new Pays(strT.nextToken(), strT.nextToken()));
-        }
-        return aLst;
+            aLst.add(new Pays(strTPays.nextToken(), strTPays.nextToken()));
+        }*/
+        return new ArrayList();//aLst;
     }
 }
--- src/main/java/dao/SkieurDao.java
+++ src/main/java/dao/SkieurDao.java
@@ -2,7 +2,7 @@ package dao;
 
 import java.util.ArrayList;
 import java.util.StringTokenizer;
-import outils.FileStr;
+//import outils.FileStr;
 import domaine.Skieur;
 import domaine.Pays;
 
@@ -11,13 +11,14 @@ public class SkieurDao {
     private static final String FICHIER_SKIEURS = "Skieurs.txt";
 
     public static ArrayList getListeSkieurs(Pays pays) {
-        String[] tabSkieurs = FileStr.read(FICHIER_SKIEURS);
+        String[] tabSkieurs = new String[3];//FileStr.read(FICHIER_SKIEURS);
+        /*tabSkieurs[0] = "kookokokokojfo";
         ArrayList aLst = new ArrayList();
         for (int i=0; i<tabSkieurs.length; i++) {
             StringTokenizer strT = new StringTokenizer(tabSkieurs[i], ";");
             Pays p = new Pays(strT.nextToken());
             if (p.equals(pays)) { aLst.add(new Skieur(Integer.parseInt(strT.nextToken()), strT.nextToken(), strT.nextToken(), pays)); }
-        }
-        return aLst;
+        }*/
+        return new ArrayList();//aLst;
     }   
 }
